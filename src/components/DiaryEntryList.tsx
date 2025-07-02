824342ea85af4c7c7d13118bebad22de696747b8
jinjinsansan <goldbenchan@gmail.com>
2025-07-02 11:13:22 +0900

    スマホ表示の日記リスト修正

--- src/components/DiaryEntryList.tsx
+++ src/components/DiaryEntryList.tsx
@@ -1,3 +1,1 @@
-@@ .. @@
--<div className="mx-auto max-w-md space-y-4">
-+<div className="w-full px-4 sm:mx-auto sm:max-w-xl space-y-4">
+<div className="w-full px-4 sm:mx-auto sm:max-w-md space-y-4">
